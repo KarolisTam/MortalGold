e5ab0d23da24168f4aa22456032cb625ff8f450c
RenaldasZ <renald.zvega@gmail.com>
2023-06-29 19:56:41 +0300

    added  two characters, char test pictures, fixed player coordination added health

--- character.py
+++ character.py
@@ -1,12 +1,32 @@
 import pygame
 
 class Character():
-    def __init__(self, x, y):
+    def __init__(self, x, y, flip, data, sprite_sheet, animation_steps):
+        self.size = data[0]
+        self.image_scale = data[1]
+        self.offset = data[2]
+        self.flip = flip
+        self.animation_list = self.load_images(sprite_sheet, animation_steps)
+        self.action = 0 #0:idle #1:run #2:jump #3:punch #4:kick #5:gethit #6:death #7:dizzy #8:victory #9:
+        self.frame_index = 0
+        self.image = self.animation_list[self.action][self.frame_index] 
         self.rect = pygame.Rect((x, y, 80, 180))
         self.vel_y = 0
         self.jump = False
         self.attacking = False
         self.attack_type = 0
+        self.health = 100
+
+    def load_images(self, sprite_sheet, animation_steps):
+        # extract images from spritesheet
+        animation_list = []
+        for y, animation in enumerate(animation_steps):
+            temp_img_list = []
+            for x in range(animation):
+                temp_img = sprite_sheet.subsurface(x * self.size, y * self.size, self.size, self.size)
+                temp_img_list.append(pygame.transform.scale(temp_img, (self.size * self.image_scale, self.size * self.image_scale)))
+            animation_list.append(temp_img_list)
+        return animation_list
 
     def move(self, screen_width, screen_height, surface, target):
         SPEED = 10
@@ -24,7 +44,6 @@ class Character():
                 dx = -SPEED
             if key[pygame.K_d]:
                 dx = SPEED
-
             # jump
             if key[pygame.K_w] and self.jump == False:
                 self.vel_y = -30
@@ -33,7 +52,6 @@ class Character():
             # Attack
             if key[pygame.K_r] or key[pygame.K_t]:
                 self.attack(surface, target)
-
                 # determine wich attact type was used
                 if key[pygame.K_r]:
                     self.attack_type = 1
@@ -54,17 +72,26 @@ class Character():
             self.jump = False
             dy = screen_height - 90 - self.rect.bottom
 
+        # ensure players face each other
+        if target.rect.centerx > self.rect.centerx:
+            self.flip = False
+        else:
+            self.flip = True
+
         # update player possition
         self.rect.x += dx
         self.rect.y += dy
 
     def attack(self, surface, target):
         self.attacking = True
-        attacking_rect = pygame.Rect(self.rect.centerx, self.rect.y, 2 * self.rect.width, self.rect.height)
+        attacking_rect = pygame.Rect(self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, self.rect.height)
         if attacking_rect.colliderect(target.rect):
             print("hit")
+            target.health -= 10
         
         pygame.draw.rect(surface, (0, 255, 0), attacking_rect)
 
     def draw(self, surface):
+        img = pygame.transform.flip(self.image, self.flip, False)
         pygame.draw.rect(surface, (255, 0, 0), self.rect)
+        surface.blit(img, (self.rect.x - (self.offset[0] * self.image_scale), self.rect.y - (self.offset[1] * self.image_scale)))
--- gameclient.py
+++ gameclient.py
@@ -13,17 +13,48 @@ pygame.display.set_caption("Mortal Gold")
 clock = pygame.time.Clock()
 FPS = 60
 
+#define colours
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+
+# define character variables
+PUTIN_SIZE = 200
+PUTIN_SCALE = 1
+PUTIN_OFFSET = [62, 26]
+PUTIN_DATA = [PUTIN_SIZE, PUTIN_SCALE, PUTIN_OFFSET]
+
+ZUCKENBERG_SIZE = 200
+ZUCKENBERG_SCALE = 3
+ZUCKENBERG_OFFSET = [63, 109]
+ZUCKENBERG_DATA = [ZUCKENBERG_SIZE, ZUCKENBERG_SCALE, ZUCKENBERG_OFFSET]
+
 # Load background image
 bg_image = pygame.image.load("assets/images/background/bg.png").convert_alpha()
 scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# Load spritesheets
+putin_sheet = pygame.image.load("assets/images/putin/sprites/putin.png").convert_alpha()
+zuckenberg_sheet = pygame.image.load("assets/images/zuckenberg/sprites/zuckenberg.png").convert_alpha()
+
+# define number of steps in each animation
+PUTIN_ANIMATION_STEPS = [10, 8, 1, 7, 7, 3, 7]
+ZUCKENBERG_ANIMATION_STEPS = [8, 8, 1, 8, 8, 3, 7]
 
 # Function for drawing background
 def draw_bg():
     screen.blit(bg_image, (0, 0))
 
+#  function for drawing character health bars
+def draw_health_bar(health, x, y):
+    ratio = health / 100
+    pygame.draw.rect(screen, WHITE, (x - 2, y - 2, 404, 34))
+    pygame.draw.rect(screen, RED, (x, y, 400, 30))
+    pygame.draw.rect(screen, YELLOW, (x, y, 400 * ratio, 30))
+
 # Create two instances of characters
-character_1 = Character(200, 450)
-character_2 = Character(700, 450)
+character_1 = Character(200, 450, False, PUTIN_DATA, putin_sheet, PUTIN_ANIMATION_STEPS)
+character_2 = Character(700, 450, True, ZUCKENBERG_DATA, zuckenberg_sheet, ZUCKENBERG_ANIMATION_STEPS)
 
 run = True
 while run:
@@ -37,10 +68,13 @@ while run:
     character_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, character_2)
     # character_2.move()
 
+    # show player stats
+    draw_health_bar(character_1.health, 20, 20)
+    draw_health_bar(character_2.health, 860, 20)
+
     # draw character
     character_1.draw(screen)
     character_2.draw(screen)
-
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
