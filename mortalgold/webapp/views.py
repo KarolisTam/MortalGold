58170158a4be8a7b8bca2f4b51397fb3ba64c49c
RenaldasZ <renald.zvega@gmail.com>
2023-07-05 23:17:51 +0300

    added navigations to the base and created bg style

--- mortalgold/webapp/views.py
+++ mortalgold/webapp/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'webapp/index.html')
+
+def htp(request):
+    return render(request, 'webapp/htp.html')
+
+def downloads(request):
+    return render(request, 'webapp/downloads.html')
+
+def tops(request):
+    return render(request, 'webapp/tops.html')
